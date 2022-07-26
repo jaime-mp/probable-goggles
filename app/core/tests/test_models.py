130d26107a111557a2031e5589b9fc7e62492758
Jaime <martinperez.jaime01@gmail.com>
2022-07-26 18:16:19 +0200

    Fixed linting error.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,7 +13,10 @@ class ModelTest(TestCase):
 
         email = "test@example.com"
         password = "testpass1234"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 

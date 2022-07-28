3f5559ecabc14d127b9b2db4c0b5fa7d42658d37
Jaime <martinperez.jaime01@gmail.com>
2022-07-28 20:22:09 +0200

    Endpoint User implemented.

---
+++ app/user/serializers.py
@@ -0,0 +1,62 @@
+"""
+Serializers for the user API View.
+"""
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
+
+
+from rest_framework import serializers
+
+from core.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
+
+    class Meta:
+        model = get_user_model()
+        fields = ["email", "password", "name"]
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        """Create and return a user with encrypted password."""
+        return self.Meta.model.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user: User = super().update(instance=instance, validated_data=validated_data)
+
+        if password:
+            user.set_password(raw_password=password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs: dict):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user: User = authenticate(
+            request=self.context.get("request"), username=email, password=password
+        )
+
+        if not user:
+            msg = _("Unable to authenticate with provided credentials.")
+            # * this is the standar way to raise errors with serializers
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
---
+++ app/user/views.py
@@ -0,0 +1,34 @@
+"""
+Views for the user API.
+"""
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    serializer_class = AuthTokenSerializer
+    # * 'rest_framework.renderers.JSONRenderer'
+    # * 'rest_framework.renderers.BrowsableAPIRenderer'
+    # * These are the DEFAULT_RENDERER_CLASSES. The second one is necessary to be able to access the ednpoint from the API GUI.
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manager the authenticated user."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user

29078d0284a650735919c60cef6b7819e2fe8a0f
Jaime <martinperez.jaime01@gmail.com>
2022-07-30 14:17:57 +0200

    Recipe endpoints created.

--- app/core/admin.py
+++ app/core/admin.py
@@ -39,3 +39,8 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     ]
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,8 @@
 """
 Database models.
 """
+from django.conf import settings
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -43,3 +45,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"  # * defines the field to be used for login.
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    time_minutes = models.IntegerField(verbose_name="Cooking time")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField(verbose_name="Recipe description", blank=True)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
